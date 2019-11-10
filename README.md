094f220951f9d28a0454ee4b0fa8e38a91fd4a76
Thierry <zazart@imac-de-thierry.home>
2019-11-10 16:20:51 +0100

    Update readme with installation and user doc.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Reason-ArturiaKeylabEssential-Remote
 
-Remote is the Propellerheadʼs protocol for communication between hardware control surfaces and Reason Studio application. This project provides Reason control remote script for Arturia Keylab Essential keyboard
+Remote is the Propellerheadʼs protocol for communication between hardware control surfaces and Reason Studio application. This project provides Reason remote script for the **DAW Command Center** surface of the Arturia Keylab Essential keyboard
 
 **Note**: I am not affiliated or associated in any way with Arturia or Propellerhead. I have created these files myself with the files and programs legally available to me.
 
@@ -9,6 +9,8 @@ Remote is the Propellerheadʼs protocol for communication between hardware contr
 ![Arturia Keylab Essential Logo](https://medias.arturia.net/images/products/keylab-essential/keylab-essential-image.png)
 
 ## Installation
+
+### Install the scripts
 
 To install the Remote scripts in your environement, the files in the `Remote` directory should be copied to your Propellerhead installation directory:
 
@@ -24,6 +26,35 @@ Macintosh HD/Library/Application Support/Propellerhead Software/Remote
 C:/ProgramData/Propellerhead Software/Remote
 ```
 
+### Setup the Arturia DAW Command Center as the control surface in Reason
+
+* Open the Preferences dialog and click the Control Surfaces tab.
+
+<img src="./images/OpenControlSurfaceTab.png" width="400">
+
+* To add manually the control surface, click the “Add manually” button. This brings up a new dialog.
+* Select "Arturia Keylab 61 Essential Control" from the Model pop-up menu. Select `Arturia KeyLab 61 Essential DAW In` for `In Port` attribut and `Arturia Keylab Essential DAW Out` for `Out Port` attribut.
+
+<img src="./images/ControlSurfaceSelection.png" width="400">
+
+### Mapping between the Aturia DAW Command Center and Reason
+
+<img src="./images/DAWCommandCenter.png" width="300">
+
+Mapping of the different buttons
+Arturia DAW Command Center | Reason Command | Comment
+-------------------------- | -------------- | --------------------------------
+Stop | Stop | Stops playback. Pressing two time will return the playback cursor to the start of the track.
+Pause/play | Play | Starts and pauses your track at the current position of the playback cursor. In pause mode, the stop button LED is on and the pause/play button LED is slightly on
+Record | Record On/off | Arms the record function in Reason. Hitting the Record button while the track is stopped will begin playback while recording. If the track is already playing, hitting Record will begin recording from the current playback cursor position.
+Loop | Loop On/off | Toggles the Loop function on and off. The loop region is set within Reason.
+Rewind | Rewind | Quickly moves the playback cursor backward.
+Fast forward | Fast Forward | Quickly moves the playback cursor forward.
+Save | Redo |  Redo your last undo action. If there is no action to redo, the button's led is off
+Undo | Undo  | Reverses your last action. If there is no action, the button's led is off
+Punch | Precount On/Off | Toggles Reason’s metronome precount on and off.
+Metro | Click On/Off | Toggles Reason’s metronome on and off.
+
 ## Ressources on Remote
 
 * [Korg Nano Kontrol](https://github.com/carlosedp/Reason-KorgNanoKontrol2-Remote) by <carlosedp@gmail.com>
@@ -31,9 +62,10 @@ C:/ProgramData/Propellerhead Software/Remote
 * [Reason Remoter](http://www.reasonremoter.uk/)
 * [Hacking Remote Files in Reason](https://www.soundonsound.com/techniques/hacking-remote-files-reason)
 
-## Other links to help development
+## Other links
 
 * [Markdown guide](https://guides.github.com/pdfs/markdown-cheatsheet-online.pdf)
 * [LUA 5.0 documentation](http://www.lua.org/manual/5.0/.)
 * [LUA Script tutorial](https://wxlua.developpez.com/tutoriels/lua/general/cours-complet/)
+* [Midi reference tables](https://www.midi.org/specifications-old/category/reference-tables)
 * [Arturia Keylab Essential](https://www.arturia.com/support/keylab-essential-start)
--- Remote/Codecs/Lua Codecs/Arturia/Keylab61 Essential Control.lua
+++ Remote/Codecs/Lua Codecs/Arturia/Keylab61 Essential Control.lua
@@ -1,8 +1,24 @@
+--[[
+	Surface:	DAW Command Center of Arturia Keylab 61 Essential
+	Developer:	Thierry Fraudet
+	Version:	1.0
+	Date:		10/11/2019
+
+]]
+
+-- init globals varaiables
+g_pause_play_button_state = "stop"
+g_pause_play_button_index = 3
+g_stop_button_index = 2
+
+g_last_input_time=-2000
+g_last_input_item=nil
+
 function remote_init(manufacturer, model)
 	local items = {
 		{name="record", input="button", output="value"},
 		{name="stop", input="button", output="value"},
-		{name="play", input="button", output="value"}, 
+		{name="play", input="button", output="value",  modes={"Play", "Pause"}}, 
 		{name="loop", input="button", output="value"},
 		{name="rew", input="button", output="value"},
 		{name="fwd", input="button", output="value"},
@@ -62,15 +78,16 @@ end
 --
 -- to set button off
 -- send 90 xx 00
-
+--
 function remote_probe()
 	-- sysexe de demande d'identification
 	local controlRequest="f0 7e 7f 06 01 f7" 
+	-- Arturia Manufacturer SysEx ID Numbers is: 00 20 6b
 	-- response if Arturia Keylab Essential 61 is F0 7E 7F 06 02 00 20 6B 02 00 05 54 3D 02 01 01 10  F7 
 	-- local controlResponse="F0 7E 7F 06 02 ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? F7"
 	local controlResponse="F0 7E 7F 06 02 00 20 6B 02 00 05 54 3D 02 01 01 10 F7"
 	
-	-- Need to be rework, for hardware withg miltiple ports must be done programaticaly (see doc page 26 & 27) 
+	-- Need to be rework, for hardware with multiple ports must be done programaticaly (see Remote SDK documentation page 26 & 27) 
 	return {
 		request=controlRequest,
 		response=controlResponse
@@ -78,13 +95,97 @@ function remote_probe()
 end
 
 function remote_on_auto_input(item_index) 
-	-- this function is called by Remote after an auto-input item message has been handled.
-	remote.trace('in remote_on_auto_input(), item_index = ' .. item_index)
+	g_last_input_time=remote.get_time_ms()
+	g_last_input_item=item_index
+end
+
+function toggle_pause_play_button_state()
+	if g_pause_play_button_state == "play" then
+		g_pause_play_button_state = "pause" 
+	else
+		g_pause_play_button_state = "play"
+	end
+	return g_pause_play_button_state
+end
+
+function remote_process_midi(event)  -- handle incoming midi event
+	-- test if pause/play is pressed
+	ret = remote.match_midi("9? 5e 7f",event)
+	if ret~=nil then
+		g_pause_play_button_state = toggle_pause_play_button_state()
+		g_last_input_index = 3
+		if g_pause_play_button_state == "play" then
+			local msg={ time_stamp=event.time_stamp, item=g_pause_play_button_index, value=1 }
+			remote.handle_input(msg)
+		else
+			local msg={ time_stamp=event.time_stamp, item=g_stop_button_index, value=1 }
+			remote.handle_input(msg)
+		end
+		return true
+	end
+
+	--test if stop is pressed
+	ret = remote.match_midi("9? 5d 7f",event)
+	if ret~=nil then
+		g_pause_play_button_state = "stop"
+		g_last_input_index = 2;
+		local msg={ time_stamp=event.time_stamp, item=g_stop_button_index, value=1}
+		remote.handle_input(msg)
+		return true
+	end
+
+	return false
+end
+
+
+function remote_set_state(changed_items) --handle incoming changes sent by Reason
+	-- for key,value in ipairs(changed_items) do
+	-- 	if value == g_pause_play_button_index then
+	-- 		g_pause_play_button_state = toggle_pause_play_button_state()
+	-- 	end
+	-- 	if value == g_stop_button_index then
+	-- 		g_pause_play_button_state = "stop"
+	-- 	end
+
+	-- 	if (value == g_pause_play_button_index) or (value ==g_stop_button_index) then
+	-- 		-- remote.trace(' pause/play button, current state is '.. g_pause_play_button_state .. ' ,value is '.. remote.get_item_value(value)..'\r')
+	-- 		-- error(' pause/play button, current state is '.. g_pause_play_button_state .. ' ,value is '.. remote.get_item_value(value)..'\r')
+	-- 	end
+	-- end
+end
+
+function remote_deliver_midi(max_bytes, port)
+	local ret_events={}
+
+	-- play_pause_button_state has 3 value: stop, pause, play
+	-- following the state, we send sysex to light the pause/play button led
+	if (g_last_input_index == 3) then
+		if g_pause_play_button_state == "play" then
+			pause_play_led_event = remote.make_midi('F0 00 20 6B 7F 42 02 00 10 5e 7f F7')
+		elseif g_pause_play_button_state == "pause" then
+			pause_play_led_event = remote.make_midi('F0 00 20 6B 7F 42 02 00 10 5e 10 F7')
+		else
+			pause_play_led_event = remote.make_midi('F0 00 20 6B 7F 42 02 00 10 5e 00 F7')
+		end
+		table.insert(ret_events,pause_play_led_event)
+	end
+
+	-- if stop is pressed, switch off the pause/play button led.
+	if (g_last_input_index == 2) then
+		pause_play_led_event = remote.make_midi('F0 00 20 6B 7F 42 02 00 10 5e 00 F7')
+		table.insert(ret_events,pause_play_led_event)
+	end
+
+	g_last_input_index = nil
+	return ret_events
 end
 
 function remote_prepare_for_use()
 	local retEvents={
-			-- switch off all the button's leds
+		-- set to Mackie controle mode
+		remote.make_midi("f0 00 20 6b 7f 42 02 00  40 51 00 F7"),
+
+		-- switch off all the button's leds
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('56',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('57',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('58',16), y=0, port=1} ),
@@ -92,16 +193,60 @@ function remote_prepare_for_use()
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5a',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5b',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5c',16), y=0, port=1} ),
-		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5d',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5e',16), y=0, port=1} ),
+		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5d',16), y=0, port=1} ),
 		remote.make_midi("f0 00 20 6b 7f 42 02 00 10 xx yy f7", { x=tonumber('5f',16), y=0, port=1} ),
+
+		-- Display message on LCD
+		make_lcd_midi_message("Reason DAW CC", "connected"),
 	}
 	return retEvents
 end
 
 function remote_release_from_use()
 	local retEvents={
+		-- Display message on LCD
+		make_lcd_midi_message("Reason DAW CC", "disconnected"),
 	}
 	return retEvents
 end
 
+-- Sysexe to send something to the display: F0 00 20 6B 7F 42 04 00 60 01 xx xx 00 02 yy yy 00 F7 where xx is on the first line and yy on the 2nd
+--   ex: send abcdefghigklmnop
+--      f0 00 20 6b 7f 42 04 00 60 01 61 62 63 64 65 66 67 68 69 6a 6b 6c 6d 6e 6f 70 00 F7 
+--   ex: send abcdefgh ET aabcdefghi
+--      f0 00 20 6b 7f 42 04 00 60 01 61 62 63 64 65 66 67 68 00 02 61 61 62 63 64 65 66 67 68 69 00 F7 
+
+function stringToHex(text)
+	local hexStringToReturn = ""
+	for i=1, string.len(text) do
+		if string.len(hexStringToReturn) > 0 then 
+			hexStringToReturn = hexStringToReturn .. " "
+		end
+		hexStringToReturn = hexStringToReturn .. string.format("%X", string.byte(text,i))   
+	end
+	return hexStringToReturn
+end
+
+function make_lcd_midi_message(line1, line2)
+	local sysex = "f0 00 20 6b 7f 42 04 00 60 01" .. stringToHex(string.sub(line1,1,16)) .. " 00 02" .. stringToHex(string.sub(line2,1,16)) .. " 00 f7"
+	local event=remote.make_midi(sysex)
+	return event
+end
+
+function trace_event(event)
+	result = "Event: "
+	result = result .. "port " .. event.port .. ", "
+	result = result .. (event.timestamp and ("timestamp " .. event.timestamp .. ", ") or "")
+	result = result .. (event.hi and ("hi " .. event.hi .. ", ") or "")
+	result = result .. (event.lo and ("lo " .. event.lo .. ", ") or "")
+	result = result .. (event.size and ("size " .. event.size .. ", ") or "")
+
+	result = result .. "data {"
+	for i=1,event.size do
+		result = result .. event[i] .. ", "
+	end
+	result = result .. "}, "
+
+	remote.trace(result)
+end
